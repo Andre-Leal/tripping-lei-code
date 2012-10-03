52fefa361e2eafa462328352ae61c5bdf11d96b4
Pedro Abranches <pedrof.abranches@gmail.com>
2012-10-03 22:56:41 +0100

    first commit with SD-proj1 code

---
+++ sd/proj1/src/data.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "data.h"
+
+struct data_t *data_create(int size) {
+    void *raw_data = malloc(size);
+    return data_create2(size, raw_data);
+}
+
+struct data_t *data_create2(int size, void *data) {
+    struct data_t *new_data;
+    new_data = (struct data_t *) malloc(sizeof(struct data_t));
+    new_data->datasize = size;
+    new_data->data = data;
+
+    return new_data;
+}
+
+void data_destroy(struct data_t *data) {
+    free(data->data);
+    free(data);
+}
+
+struct data_t *data_dup(struct data_t *data) {
+    void *new_raw_data = malloc(data->datasize);
+    memcpy(new_raw_data, data->data, data->datasize);
+
+    struct data_t *new_data = data_create2(data->datasize new_raw_data);
+
+    return new_data;
+}
---
+++ sd/proj1/src/entry.c
@@ -0,0 +1,42 @@
+#include "entry.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+/* Funcao que cria um novo par chave-valor (isto e, que inicializa
+* a estrutura e aloca a memoria necessaria).
+*/
+struct entry_t *entry_create(char *key, struct data_t *data) {
+
+    struct entry_t *new_entry = (struct entry_t *) malloc(sizeof(struct entry_t));
+    new_entry->timestamp = 0;     
+    new_entry->key = key;     
+    new_entry->value = data;
+
+    return new_entry;
+}
+
+/* Funcao que destroi um par chave-valor e liberta toda a memoria.
+*/
+void entry_destroy(struct entry_t *entry) {
+
+    free(entry->key);
+    data_destroy(entry->value);   
+    free(entry);
+
+}
+
+/* Funcao que duplica um par chave-valor. */
+struct entry_t *entry_dup(struct entry_t *entry) {
+
+    char *key_copy = malloc(strlen(entry->key)+1);
+    strcpy(key_copy , entry->key);
+
+    struct data_t *value_copy = malloc(sizeof(struct data_t));
+    value_copy = data_dup(entry->value);
+
+    struct entry_t *new_entry = entry_create(key_copy , value_copy);
+
+    return new_entry;
+}

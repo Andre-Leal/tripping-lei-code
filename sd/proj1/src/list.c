7a7c4b55bee1efc1eb18f5b990d8cb0fe831bc76
Pedro Abranches <pedrof.abranches@gmail.com>
2012-10-06 16:26:12 +0100

    indent list.c, use 4 spaces instead of tabs

--- sd/proj1/src/list.c
+++ sd/proj1/src/list.c
@@ -7,19 +7,19 @@
 
 
 /* Cria uma nova lista. Em caso de erro, retorna NULL.
- */
+*/
 struct list_t *list_create() {
-	
-	struct list_t *list = (struct list_t *) malloc(sizeof(struct list_t));
-	list->size = 0;
-	list->header = NULL;
 
-	if( !list) {
-		perror("Error malloc\n");	
-		return NULL;	
-	}
+    struct list_t *list = (struct list_t *) malloc(sizeof(struct list_t));
+    list->size = 0;
+    list->header = NULL;
 
-	return list;	
+    if( !list) {
+        perror("Error malloc\n");
+        return NULL;
+    }
+
+    return list;
 
 }
 
@@ -29,20 +29,20 @@ struct list_t *list_create() {
  */
 int list_destroy(struct list_t *list) {
 
-	erase_all_memory(list->header);
-	free(list);
+    erase_all_memory(list->header);
+    free(list);
 }
 
 /*
-Method that recursively frees all the memory space reserved.
-*/
+   Method that recursively frees all the memory space reserved.
+   */
 void erase_all_memory (struct node_t *node) {
-	
-	if (node != NULL) {
-		erase_all_memory(node->next);
-		entry_destroy(node->entry);
-		free(node);
-	}
+
+    if (node != NULL) {
+        erase_all_memory(node->next);
+        entry_destroy(node->entry);
+        free(node);
+    }
 }
 
 /* Adiciona uma entry na lista. Como a lista deve ser ordenada, 
@@ -51,51 +51,51 @@ void erase_all_memory (struct node_t *node) {
  */
 int list_add(struct list_t *list, struct entry_t *entry) {
 
-	struct node_t *new_node = (struct node_t *) malloc(sizeof(struct node_t));
-	new_node->entry = entry;
-	new_node->next = NULL;
+    struct node_t *new_node = (struct node_t *) malloc(sizeof(struct node_t));
+    new_node->entry = entry;
+    new_node->next = NULL;
 
-	//Add if list is empty
+    //Add if list is empty
     if (list->size == 0) {
         list->header = new_node;
         list->size++;
         return 0;
     }
 
-	//add if list has onde element
-	if(list->size == 1) {
-		if (strcmp(new_node->entry->key , list->header->entry->key) < 0) {
-			new_node = list->header;
-			list->header = new_node;		
-		}
-		else {
-        	new_node->next = list->header->next;
-			list->header->next = new_node;
-		}
-		list->size++;
-		return 0;
-	}
+    //add if list has onde element
+    if(list->size == 1) {
+        if (strcmp(new_node->entry->key , list->header->entry->key) < 0) {
+            new_node = list->header;
+            list->header = new_node;
+        }
+        else {
+            new_node->next = list->header->next;
+            list->header->next = new_node;
+        }
+        list->size++;
+        return 0;
+    }
 
     struct node_t *curr_node;
-	curr_node = list->header;
+    curr_node = list->header;
 
-	if (strcmp(new_node->entry->key , curr_node->entry->key) < 0) {
+    if (strcmp(new_node->entry->key , curr_node->entry->key) < 0) {
         new_node->next = curr_node->next;
-		curr_node->next = new_node;
-	}
+        curr_node->next = new_node;
+    }
 
     while (curr_node->next != NULL) {
-    	if (strcmp(new_node->entry->key , curr_node->next->entry->key) < 0) {
+        if (strcmp(new_node->entry->key , curr_node->next->entry->key) < 0) {
             new_node->next = curr_node->next;
             curr_node->next = new_node;
             list->size++;
             return 0;
         }
-		curr_node = curr_node->next;
+        curr_node = curr_node->next;
     }
- 	list->size++;
-	curr_node->next = new_node;
-	return 0;
+    list->size++;
+    curr_node->next = new_node;
+    return 0;
 
 }
 
@@ -105,20 +105,20 @@ int list_add(struct list_t *list, struct entry_t *entry) {
  */
 int list_remove(struct list_t *list, char *key) {
 
-	struct node_t *curr_node = list->header;
+    struct node_t *curr_node = list->header;
 
-	if (list->size == 0) {return -1;}
-	
-	while (curr_node != NULL) {
-		if(strcmp(key , curr_node->entry->key)==0) {
-			curr_node = curr_node->next;
-			list->size--;
-			return 0;
-		}
-		curr_node = curr_node->next;			
-	}
-	
-	return -1;
+    if (list->size == 0) {return -1;}
+
+    while (curr_node != NULL) {
+        if(strcmp(key , curr_node->entry->key)==0) {
+            curr_node = curr_node->next;
+            list->size--;
+            return 0;
+        }
+        curr_node = curr_node->next;
+    }
+
+    return -1;
 }
 
 
@@ -129,15 +129,15 @@ int list_remove(struct list_t *list, char *key) {
  * a responsável por libertar a memoria ocupada pelo elemento.
  */
 struct entry_t *list_get(struct list_t *list, char *key) {
-	
-	struct node_t *node = list->header;
 
-	while(node != NULL) {
-		if(strcmp(key , node->entry->key)==0) {
-			return node->entry;
-		}
-		node = node->next;		
-	}
+    struct node_t *node = list->header;
+
+    while(node != NULL) {
+        if(strcmp(key , node->entry->key)==0) {
+            return node->entry;
+        }
+        node = node->next;
+    }
 }
 
 /* Retorna o tamanho (numero de elementos) da lista 
@@ -145,7 +145,7 @@ struct entry_t *list_get(struct list_t *list, char *key) {
  */
 int list_size(struct list_t *list) {
 
-	return list->size;
+    return list->size;
 
 }
 
@@ -153,31 +153,31 @@ int list_size(struct list_t *list) {
  * lista, com um ultimo elemento a NULL.
  */
 char **list_get_keys(struct list_t *list) {
-	
-	char **array_key = malloc((list->size+1) * sizeof(char *));
-	
-	struct node_t *node = list->header;
-	int i;
-	for(i=0;i<list->size;i++) {
-		array_key[i] = malloc(strlen(node->entry->key)*sizeof(char *));
-		strcpy(array_key[i] , node->entry->key);
-		node = node->next;
-	}
-	array_key[i] = NULL;
-	
-	return array_key;
+
+    char **array_key = malloc((list->size+1) * sizeof(char *));
+
+    struct node_t *node = list->header;
+    int i;
+    for(i=0;i<list->size;i++) {
+        array_key[i] = malloc(strlen(node->entry->key)*sizeof(char *));
+        strcpy(array_key[i] , node->entry->key);
+        node = node->next;
+    }
+    array_key[i] = NULL;
+
+    return array_key;
 }
 
 /* Liberta a memoria alocada por list_get_keys
- */
+*/
 void list_free_keys(char **keys) {
 
-	int i = 0;
-	
-	while(keys[i] != NULL) {
-		free(keys[i]);
-		i++;
-	}
-	free(keys);
+    int i = 0;
+
+    while(keys[i] != NULL) {
+        free(keys[i]);
+        i++;
+    }
+    free(keys);
 
 }
